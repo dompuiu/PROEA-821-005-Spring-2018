fb5a32b20c702e9b1e454464c8624ce23b805399
Serban Stancu <stancu@adobe.com>
2018-04-14 08:43:58 -0600

    Add bias as first column.

--- HW2/data_set_loader.py
+++ HW2/data_set_loader.py
@@ -12,17 +12,14 @@ class DataSetLoader:
 
         with open(self.filename, 'r') as ins:
             for line in ins:
-                feature_row = [0 for _ in range(self.size)]
-
-                # Add Bias
-                feature_row.append(1)
+                feature_row = [1 if i == 0 else 0 for i in range(self.size + 1)]
 
                 pieces = deque(line.split(' '))
                 labels.append(int(pieces.popleft()))
 
                 for feature_data in pieces:
                     feature_parts = feature_data.split(':')
-                    feature_row[int(feature_parts[0]) - 1] = float(feature_parts[1])
+                    feature_row[int(feature_parts[0])] = float(feature_parts[1])
 
                 features.append(feature_row)
 
